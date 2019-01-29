a1939c34180596f18bd597e54a7ce8874c318b32
OoiTengHong <34025654+OoiTengHong@users.noreply.github.com>
2019-01-29 14:36:51 +0800

    update homepage

--- 1.HomePage.php
+++ 1.HomePage.php
@@ -25,18 +25,18 @@
 	        <a class="nav-link" href="1.HomePage.php"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
-<<<<<<< HEAD
+
 	        <a class="nav-link" href="6.Word Dictionary.php"><i class="fas fa-book"></i> Thesaurus</a>
 	      </li>
 	      <li class="nav-item">
 	        <a class="nav-link" href="7.Quiz.php"><i class="far fa-question-circle"></i> Quiz</a>
 	      </li>
 	      <li class="nav-item">
-=======
+
 	        <a class="nav-link" href="Quiz1.php"><i class="far fa-question-circle"></i> Quiz</a>
 	      </li>
 	      <li class="nav-item">
->>>>>>> 4b2ae66322a5611cb6e8f1310c48b9ecae295c93
+
 	        <a class="nav-link" href="Translate.html"><i class="fas fa-language"></i> Google Translate</a>
 	      </li>
 	    </ul>
