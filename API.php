df96c25d37b200b7ed31907359ab2cd2854ed995
hohanwei97 <46925431+hohanwei97@users.noreply.github.com>
2019-01-29 12:39:51 +0900

    Update

--- API.php
+++ API.php
@@ -1,8 +1,60 @@
 <html>
 <head>
+	<meta charset="UTF-8">
+	<title>Thesaurus Online Learning Tools</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+	<style>
+		body {
+			font-family: "Roboto", sans-serif;
+			background-size: cover;
+			background-color: grey;
+		}
+	</style>
+	<link rel="stylesheet" type="text/css" href="5.style.css">
 </head>
 <form action="API.php" method="post">
-<body> 
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+   	<div class="container">
+   	<a class="navbar-brand" href="#">Word Dictionary</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="1.HomePage.php"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#"><i class="far fa-question-circle"></i> Quiz</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#"><i class="fas fa-language"></i> Google Translate</a>
+	      </li>
+	    </ul>
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item">
+	        <a class="nav-link" href="2.Sign In.php"><i class="fas fa-user"></i> Sign In</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="4.Sign Up.php"><i class="fas fa-user-plus"></i> Sign Up</a>
+	      </li>
+	    </ul>
+	  </div>
+   	</div>
+	  
+	</nav>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 Enter the word: <input type="text" name="word">
 <br>
 <br>
